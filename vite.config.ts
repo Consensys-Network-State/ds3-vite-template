1a1035782cf41564604aef0c84d6c6ea7658142a
Leif DeJong <leifdejong@gmail.com>
2025-06-02 14:14:14 +0200

    Upgrading ui-config to 0.0.5

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import cui from '@consensys/ui-config/vite';
 import themeConfig from "./theme.config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      // Polyfill Buffer
-      buffer: 'buffer/',
-    },
-  },
   plugins: [
     cui(themeConfig),
   ],
